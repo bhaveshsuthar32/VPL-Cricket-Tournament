24b74a9b2905b5afe934f7d55953d2028f5a3d43
bhaveshsuthar32 <bagrabhavesh426@gmail.com>
2024-08-20 17:33:22 +0530

    init commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { BrowserRouter,  Routes, Route } from 'react-router-dom'
 import Home from './User/pages/home'
+import Login from './User/pages/login'
+import SignUp from './User/pages/sign'
 
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
       <BrowserRouter>
         <Routes>
          <Route path='/' element={<Home />} /> 
+         <Route path='/login' element = {<Login /> } />
+         <Route path='/signup' element = {<SignUp /> } />
         </Routes>
       </BrowserRouter>
     </>
--- frontend/src/User/pages/home.jsx
+++ frontend/src/User/pages/home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 export default function home() {
   return (
     <>
       <h1>welcome home page</h1>
+      <Link to={'/login'}> login button</Link>
     </>
   )
 }
