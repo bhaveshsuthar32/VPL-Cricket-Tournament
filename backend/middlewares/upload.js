a1f1aae60329cf92bfb739d012e56ccfe7fe4b41
bhaveshsuthar32 <bagrabhavesh426@gmail.com>
2024-08-29 22:58:38 +0530

    init commit

---
+++ backend/middlewares/upload.js
@@ -0,0 +1,25 @@
+const cloudinary = require("cloudinary").v2 ;
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUD_NAME, 
+    api_key: process.env.API_KEY, 
+    api_secret: process.env.API_SECRET 
+});
+
+
+const uploadFile = async (filePath) => {
+    try {
+        const result = await cloudinary.uploader.upload(filePath)
+        console.log(result);
+        return result ;
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+exports.module = {
+    uploadFile
+}
---
+++ backend/models/addSponsor.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+
+const foodSponsor = new mongoose.Schema({
+    sponDay : {
+        type : String,
+        required : false,
+    },
+    fullName : {
+        type : String,
+        required : false,
+    },
+    Village : {
+        type : String,
+        required : false,
+    },
+    Amount : {
+        type : String,
+        required : false,
+    },
+    spImage : {
+        type : String,
+        required : false,
+    },
+})
+
+const foodspon = mongoose.model('foodSponsor', foodSponsor);
+
+
+
+
+const otherSponsor = new mongoose.Schema({
+    sponType : {
+        type : String,
+        required : false,
+    },
+    fullName : {
+        type : String,
+        required : false,
+    },
+    Village : {
+        type : String,
+        required : false,
+    },
+    Amount : {
+        type : String,
+        required : false,
+    },
+    spImage : {
+        type : String,
+        required : false,
+    },
+})
+
+const otherSpon = mongoose.model('otherSponsor', otherSponsor);
+
+exports.model = {
+    foodspon,
+    otherSpon,
+}
