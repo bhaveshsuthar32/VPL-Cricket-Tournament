f9636f24f122eeaf6183db5172f6fa711ab579ac
Bhavesh Suthar <127782206+bhaveshsuthar32@users.noreply.github.com>
2024-09-23 15:17:32 +0530

    feat : edit page

--- client/src/pages/admin/Pages/SType/index.jsx
+++ client/src/pages/admin/Pages/SType/index.jsx
@@ -59,7 +59,7 @@ const handleDeleteClick = (sponserId) => {
             </div>
             <div className="flex items-center justify-end gap-x-6">
               <Link
-                to={"/dashboard/sponsertype"}
+                to={"/dashboard/sponsertypeform"}
                 className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 ADD
