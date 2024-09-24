8a41d328ef2973af568904611d64c69b4dfe2da0
Bhavesh Suthar <127782206+bhaveshsuthar32@users.noreply.github.com>
2024-09-24 06:17:20 +0530

    Update router.js

--- server/routes/router.js
+++ server/routes/router.js
@@ -4,30 +4,16 @@ const multer = require('multer');
 
 
 
-Configure multer for file handling
+// Configure multer for file handling
 const upload = multer({ 
     storage: multer.diskStorage({}),
     limits: {fileSize: 500000}
 }); // Temporary storage for uploaded files
 
-// const upload = multer({
-//     storage: multer.memoryStorage({}),
-//     limits: { fileSize: 500000 },
-//   });
-  
 
 const { signUser, loginUser, getUser, editUserCaptain } = require('../controllers/userController');
-const { addSponsorType, getSponsorType, addFoodSpon, addOtherSpon,getFoodSpon,getOtherSpon ,deleteSponsorType,deleteFoodSpon,deleteOtherSpon} = require('../controllers/advertiserCont');
-// const { addAdvertise, getAdvertiser,  deleteAdvertiser, getAdvertiserById, editAdvertiser } = require('../controllers/advertiserCont');
-
-const {
-  addAdvertise,
-  getAdvertiser,
-  deleteAdvertiser,
-  editAdvertiser,
-  getAdvertiserById,
-} = require("../controllers/advertiserCont");
-
+const { addSponsorType, getSponsorType, addFoodSpon, addOtherSpon,getFoodSpon,getOtherSpon ,deleteSponsorType,deleteFoodSpon,deleteOtherSpon} = require('../controllers/sponsorController');
+const { addAdvertise, getAdvertiser,  deleteAdvertiser } = require('../controllers/advertiserCont');
 
 router.post("/sign", signUser);
 router.post("/login", loginUser);
@@ -49,23 +35,9 @@ router.post('/addOtherSpon', upload.single('spOtherImage'), addOtherSpon);
 router.get('/getOtherSpon', getOtherSpon);
 router.delete("/other-sponser/:id", deleteOtherSpon);
 
-// router.post('/addAdvertise', upload.single('shopLogo'), addAdvertise);
-// router.get('/getAdvertise',  getAdvertiser);
-// router.delete("/advertiser/:id", deleteAdvertiser);
-
-
-// router.post('/addAdvertise', upload.single('shopLogo'), addAdvertise);
-// router.get('/getAdvertise',  getAdvertiser);
-// router.get("/getAdvertise/:id", getAdvertiserById);
-// router.delete("/advertiser/:id", deleteAdvertiser);
-// router.patch("/advertiser/:id", upload.single("shopLogo"), editAdvertiser);
-
-// Advertiser Routes -----------------
-router.post("/addAdvertise", upload.single("shopLogo"), addAdvertise);
-router.get("/getAdvertise", getAdvertiser);
-router.get("/getAdvertise/:id", getAdvertiserById);
+router.post('/addAdvertise', upload.single('shopLogo'), addAdvertise);
+router.get('/getAdvertise',  getAdvertiser);
 router.delete("/advertiser/:id", deleteAdvertiser);
-router.patch("/advertiser/:id", upload.single("shopLogo"), editAdvertiser);
 
 
 
