30a3045a6f2917bd1f7cdb7b73a2d791f4878dae
bhavesh suthar <bagrabhavesh426@gmail.com>
2024-09-28 00:41:54 +0530

    feat : set path

--- client/src/pages/admin/components/Sidebar/index.jsx
+++ client/src/pages/admin/components/Sidebar/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import  { useState, useEffect, useRef } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 export default function Sidebar() {
