3e39a9b1b81fe668d6f0d9a37f70bb9dcf711574
bhavesh suthar <bagrabhavesh426@gmail.com>
2024-09-29 01:28:40 +0530

    feat : add team and profile api

--- client/src/api/index.jsx
+++ client/src/api/index.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//  const URL = "http://localhost:4000"
-const URL = "https://vpl-cricket-tournament-server.vercel.app"
+ const URL = "http://localhost:4000"
+// const URL = "https://vpl-cricket-tournament-server.vercel.app"
 
 
 export const signData = async(data)=>{
@@ -21,132 +21,6 @@ export const loginData = async (data) => {
       throw error; 
     }
   };
-
-
-// export const getUser = async () => {
-//     try {
-//       return await axios.get(`${URL}/user`);   
-//     } catch (error) {
-//       console.log("Error :- " , error);
-//     }
-//   };
-
-//   export const updateCaptain = async (data) => {
-//     try {
-//       return await axios.patch(`${URL}/editCaptain`, data); 
-//     } catch (error) {
-//       console.log("Error :- ", error)
-//     }
-// };
-
-
-// export const addFoodSP = async(data) => {
-//   try {
-//     return await axios.post(`${URL}/addFoodSpon`, data);
-//   } catch (error) {
-//     console.log("Error :- ", error);
-//   }
-// }
-
-
-// export const getFoodSP = async () => {
-//   try {
-//     return await axios.get(`${URL}/getFoodSpon`);   
-//   } catch (error) {
-//     console.log("Error :- " , error);
-//   }
-// };
-
-
-// export const deleteFoodSpon = async (id)  => {
-//   try {
-//     return await axios.delete(`${URL}/food-sponser/${id}`);
-//   } catch (error) {
-
-//     console.log("Error : ", error);
-//   }
-// };
-
-
-
-// export const addOtherSP = async(data) => {
-//   try {
-//     return await axios.post(`${URL}/addOtherSpon`, data);
-//   } catch (error) {
-//     console.log("Error :- ", error);
-//   }
-// }
-
-
-// export const getOtherSP = async () => {
-//   try {
-//     return await axios.get(`${URL}/getOtherSpon`);   
-//   } catch (error) {
-//     console.log("Error :- " , error);
-//   }
-// };
-
-// export const deleteOtherSpon = async (id) => {
-//   try {
-//     return await axios.delete(`${URL}/other-sponser/${id}`);
-//   } catch (error){
-//     console.log("Error : ", error);
-//   }
-// }
-
-
-
-// export const addAdvertise = async(data) => {
-//   try {
-//     return await axios.post(`${URL}/addAdvertise`, data);
-//   } catch (error) {
-//     console.log("Error :- ", error);
-//   }
-// }
-
-
-// export const getAdvertiser = async () => {
-//   try {
-//     return await axios.get(`${URL}/getAdvertise`);   
-//   } catch (error) {
-//     console.log("Error :- " , error);
-//   }
-// };
-
-// export const deleteAdvertiser = async (id) => {
-//   try {
-//     return await axios.delete(`${URL}/advertiser/${id}`);
-//   } catch (error){
-//     console.log("Error : ", error);
-//   }
-// }
-
-
-// export const addSponsorType = async(data) => {
-//   try {
-//     return await axios.post(`${URL}/sponsorType`, data);
-//   } catch (error) {
-//     console.log("Error :- ", error);
-//   }
-// }
-
-// export const getSponsorType = async () => {
-//   try {
-//     return await axios.get(`${URL}/getSponsorType`);   
-//   } catch (error) {
-//     console.log("Error :- " , error);
-//   }
-// };
-
-// export const deleteSponsorType = async (id) => {
-//   try {
-//     return await axios.delete(`${URL}/deletesponsortype/${id}`);
-//   } catch (error) {
-//     console.log("Error : ", error);
-//   }
-// }
-
-
 
 
 
@@ -410,6 +284,21 @@ export const getUser = async () => {
   }
 };
 
+export const getUserProfile = async () => {
+  const token = localStorage.getItem('token'); // Retrieve token from localStorage
+  try {
+    return await axios.get(`${URL}/user-profile`, {
+      headers: {
+        Authorization: `Bearer ${token}`, // Pass the token in the Authorization header
+      },
+    });
+  } catch (error) {
+    console.log("Error fetching user profile: ", error);
+    throw error;
+  }
+};
+
+
 export const updateCaptain = async (data) => {
   try {
     return await axios.patch(`${URL}/editCaptain`, data); 
@@ -418,7 +307,6 @@ export const updateCaptain = async (data) => {
   }
 };
 
-// dilip api ----------
 
 export const createTeam = async(data) => {
   try {
