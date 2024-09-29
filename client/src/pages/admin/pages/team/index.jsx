13399557645fd184abd85777c89a19bb4e3f84f5
bhavesh suthar <bagrabhavesh426@gmail.com>
2024-09-29 14:26:33 +0530

    feat : remove add button

--- client/src/pages/admin/pages/team/index.jsx
+++ client/src/pages/admin/pages/team/index.jsx
@@ -62,12 +62,12 @@ export default function Team() {
               <h1 className="font-bold text-lg text-gray-600">Teams</h1>
             </div>
             <div className="flex items-center justify-end gap-x-6">
-              <Link
+              {/* <Link
                 to={"/admin/hall-of-fame-form"}
                 className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 ADD
-              </Link>
+              </Link> */}
             </div>
           </div>
           <div className="overflow-x-auto ">
