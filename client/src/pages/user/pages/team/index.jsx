18f58bcf67d81157072c53d599b092d75f8e4940
Bhavesh Suthar <127782206+bhaveshsuthar32@users.noreply.github.com>
2024-10-12 10:36:57 +0530

    feat : modify page

--- client/src/pages/user/pages/team/index.jsx
+++ client/src/pages/user/pages/team/index.jsx
@@ -19,11 +19,10 @@ export default function Team() {
 
     fetchTeams();
   }, []);
-
   return (
     <>
       <Navbar />
-      <section className="bg-base-100 h-auto py-16">
+<section className="bg-base-100 h-auto py-16">
         <div className="mx-16">
           <div className="mx-auto pb-16 max-w-4xl">
             <h1 className="md:text-[40px] text-xl font-bold text-center">
@@ -34,7 +33,7 @@ export default function Team() {
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
               {teams.map((team) => (
                 <Link to={`/teamview/${team._id}`} key={team._id}>
-                  <div className="card card-compact bg-base-100 w-full sm:w-full lg:w-94 shadow-xl hover:shadow-2xl transition-shadow duration-300">
+                  <div className="card card-compact bg-base-100 md:w-80 md:h-80 shadow-xl hover:shadow-2xl transition-shadow duration-300">
                     <figure>
                       <img
                         src={
