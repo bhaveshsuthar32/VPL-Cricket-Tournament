d785c3212c6a22983e99e72bea98d476bf7318d8
bhavesh suthar <bagrabhavesh426@gmail.com>
2024-10-24 18:14:10 +0530

    feat : edit page

--- server/index.js
+++ server/index.js
@@ -41,8 +41,6 @@
 // const mongodb = require('./config/db');
 // const Route = require('./routes/router');
 // const cors = require('cors');
-<<<<<<< HEAD
-=======
 
 // dotenv.config();
 
@@ -106,35 +104,14 @@ const dotenv = require('dotenv');
 const mongodb = require('./config/db');
 const Route = require('./routes/router');
 const cors = require('cors');
->>>>>>> 10de8bd3d23abb5c35d2d3c8693680e90963941d
 
-// dotenv.config();
+dotenv.config();
 
-// const app = express();
-// const port = process.env.PORT || 5000; 
+const app = express();
+const port = process.env.PORT || 5000; 
 
-// mongodb();
+mongodb();
 
-<<<<<<< HEAD
-// // Set up CORS
-// const server = http.createServer(app);
-// const io = new Server(server, {
-//   cors: {
-//     // origin: "http://localhost:5173",
-//     origin: "https://vpl-cricket-tournament.vercel.app",
-//     methods: ["GET", "POST"]
-//   }
-// });
-
-
-// app.use(cors(
-//     {
-//         origin: ["https://vpl-cricket-tournament.vercel.app"]
-//     }
-//  ))
-
-// // app.use(cors());
-=======
 // Set up CORS
 const server = http.createServer(app);
 const io = new Server(server, {
@@ -147,30 +124,9 @@ const io = new Server(server, {
 app.use(cors({
   origin: "https://vpl-cricket-tournament.vercel.app" // Ensure CORS is set for the client URL
 }));
->>>>>>> 10de8bd3d23abb5c35d2d3c8693680e90963941d
 
-// app.use(express.json());
+app.use(express.json());
 
-<<<<<<< HEAD
-// // add socket.io instance to req object
-// app.use((req, res, next) => {
-//   req.io = io;
-//   next();
-// });
-
-// // Socket.IO connection event
-// io.on('connection', (socket) => {
-//     console.log('New client connected');
-
-//     socket.on('disconnect', () => {
-//         console.log('Client disconnected');
-//     });
-// });
-
-// app.get("/checkserver", (req, res) => {
-//     res.send("Welcome to the backend page");
-// });
-=======
 // Add socket.io instance to req object
 app.use((req, res, next) => {
   req.io = io;
@@ -189,19 +145,10 @@ io.on('connection', (socket) => {
 app.get("/checkserver", (req, res) => {
   res.send("Welcome to the backend page");
 });
->>>>>>> 10de8bd3d23abb5c35d2d3c8693680e90963941d
 
-// app.use('/', Route); 
+app.use('/', Route); 
 
-<<<<<<< HEAD
-// server.listen(port, "localhost", () => {
-//     console.log(`Server started: http://localhost:${port}`);
-// });
-
-const socket = io('https://vpl-cricket-tournament-server.vercel.app');
-=======
 // Listen on all IPs by removing the "localhost" restriction
 server.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
->>>>>>> 10de8bd3d23abb5c35d2d3c8693680e90963941d
