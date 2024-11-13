9b0ec2ae476eb723d314908f8dc46e8e02a37d94
bhavesh suthar <bagrabhavesh426@gmail.com>
2024-11-13 11:04:10 +0530

    refactor : edit cors url

--- server/index.js
+++ server/index.js
@@ -72,7 +72,7 @@ mongodb();
 const io = new Server(server, {
   cors: {
     origin: [
-      "http://localhost:5173", // Local client
+      // "http://localhost:5173", // Local client
       "https://vpl-cricket-tournament.vercel.app" // Production client
     ],
     methods: ["GET", "POST"]
@@ -81,7 +81,7 @@ const io = new Server(server, {
 
 app.use(cors({
   origin: [
-    "http://localhost:5173",
+    // "http://localhost:5173",
     "https://vpl-cricket-tournament.vercel.app"
   ]
 }));
